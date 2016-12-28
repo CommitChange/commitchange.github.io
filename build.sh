ed3986460249e0eb13847011ad9bdebdddbda3d1
yutaka_houlette <yutakahoulette@gmail.com>
2016-12-28 15:52:18 -0800

    Update build script

--- build.sh
+++ build.sh
@@ -8,5 +8,7 @@ echo "building css into dist/build.css"
 postcss -c postcss.js -l && cp build.css dist/build.css
 echo "browserifying js into dist/index.js..."
 browserify -t babelify index.js | uglifyjs > dist/index.js
+echo "copying markdown into dist/markdown"
+cp -R markdown dist/markdown 
 echo "done. now cd into dist, git add, git commit and push built files"    
 
